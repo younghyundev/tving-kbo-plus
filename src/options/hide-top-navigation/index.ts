2afa3d79f53be0220d8060348a6bf49611fcb1fa
yougncheon <kimyoungcheon97@gmail.com>
2026-09-12 17:20:44 +0900

    fix: show top navigation while scrolling

--- src/options/hide-top-navigation/index.ts
+++ src/options/hide-top-navigation/index.ts
@@ -1,6 +1,8 @@
 import selectors from "../../constant/selectors";
 
 const STYLE_ID = "kbo-plus-hide-top-navigation-style";
+const HIDDEN_ATTRIBUTE = "data-kbo-plus-top-navigation-hidden";
+const SCROLL_THRESHOLD = 24;
 
 export function hideTopNavigation(enabled: boolean) {
   if (!enabled || document.getElementById(STYLE_ID)) return;
@@ -8,16 +10,34 @@ export function hideTopNavigation(enabled: boolean) {
   const style = document.createElement("style");
   style.id = STYLE_ID;
   style.textContent = `
-    ${selectors.SPORTS_GNB_WRAPPER},
-    ${selectors.SPORTS_TYPE_HEADER_WRAPPER} {
+    :root[${HIDDEN_ATTRIBUTE}] ${selectors.SPORTS_GNB_WRAPPER},
+    :root[${HIDDEN_ATTRIBUTE}] ${selectors.SPORTS_TYPE_HEADER_WRAPPER} {
       display: none !important;
     }
 
-    ${selectors.SPORTS_GAME_ROOT} {
+    :root[${HIDDEN_ATTRIBUTE}] ${selectors.SPORTS_GAME_ROOT} {
       --sports-header-height: 0px !important;
       --sports-gnb-height: 0px !important;
       --sports-type-header-height: 0px !important;
     }
   `;
   document.head.appendChild(style);
+
+  let hidden: boolean | undefined;
+  const updateNavigation = () => {
+    const shouldHide = window.scrollY <= SCROLL_THRESHOLD;
+    if (shouldHide === hidden) return;
+
+    document.documentElement.toggleAttribute(HIDDEN_ATTRIBUTE, shouldHide);
+    hidden = shouldHide;
+  };
+
+  updateNavigation();
+  window.addEventListener("scroll", updateNavigation, { passive: true });
+
+  return () => {
+    window.removeEventListener("scroll", updateNavigation);
+    document.documentElement.removeAttribute(HIDDEN_ATTRIBUTE);
+    style.remove();
+  };
 }
